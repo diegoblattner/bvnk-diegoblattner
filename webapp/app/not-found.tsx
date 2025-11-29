ff0b409f85392b3f4d31a4e05876888979a2815a
Diego Blattner <>
2025-11-29 12:38:25 +0000

    not found page with error panel

--- webapp/app/not-found.tsx
+++ webapp/app/not-found.tsx
@@ -1,14 +1,8 @@
-import { ExclamationIcon, Panel } from "ui-kit";
+import { ErrorPanel } from "ui-kit";
 
 export default function NotFoundPage() {
 	return (
-		<Panel
-			className="sm:py-[68px] sm:px-[90px]"
-			preheading={
-				<div className="mx-auto">
-					<ExclamationIcon />
-				</div>
-			}
+		<ErrorPanel
 			heading="Not found"
 			description="The requested page was not found."
 		/>
